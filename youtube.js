887b7fda94b4b90f6cc29d1c7e0a47a79c8b9ff7
Shunxian Cai <shucai@microsoft.com>
2016-12-28 11:40:00 -0800

    step2, but still not perfect

--- youtube.js
+++ youtube.js
@@ -1,2 +1,6 @@
-document.getElementById('watch7-sidebar-modules').style.display="none";
-console.log("script ran");
+document.getElementsByTagName('html')[0].style.display="none";
+window.onload = function(){
+    document.getElementById('watch7-sidebar-contents').style.display="none";
+    document.getElementsByTagName('html')[0].style.display="block";
+    console.log("script ran");
+}
