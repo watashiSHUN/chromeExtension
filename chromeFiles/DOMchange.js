6a1a4af3cf96c48a4f9263c2ea6d6d9a8bd3860a
ShunXian Cai <shucai@microsoft.com>
2017-05-02 14:55:10 -0700

    use monkeypatch remove()

--- chromeFiles/DOMchange.js
+++ chromeFiles/DOMchange.js
@@ -1,23 +1,35 @@
 var blockPublisher = new Set([
 "WehSing",
 "DOTA2_YYF官方频道",
-"AdmiralBulldog"
+"AdmiralBulldog",
+"电影黑匣子",
+"优酷"
 ]);
+
+// TODO monkeypatch remove Method
+var oldRemove = Element.prototype.remove;
+Element.prototype.remove = function(){
+    // XXX this.oldRemove(arguments) == this['oldRemove'], no such property
+    var s1 = this.id ? '<id: ' + this.id + '>': '';
+    var s2 = this.className ? '<class:' + this.className + '>': '';
+    console.log('remove ' + s1 + s2 + ' successfully');
+    oldRemove.apply(this,arguments);
+}
 
 function hideContent() {
     // youtube main page
     try{
-        hideRecommandations();
+        document.getElementById('feed').remove();
         return true;
     }catch(e){
         // if it doesn't have it, we don't need to hide
-        console.log("hide main page unsuccessful");
+        console.log("hide main page unsuccessful: " + e);
     }
 
     // youtube video playing page
     try{
-        hideSideBarRecommandations();
         // if try block failed at this step -> not a video playing page
+        document.getElementById('watch7-sidebar').remove();
         var user = getPublisher();
         // if black list -> hide player
         if(blockPublisher.has(user)){
@@ -37,53 +49,18 @@ function hideContent() {
     return false;
 }
 
-function hideRecommandations(){
-    document.getElementById('feed').remove();
-    console.log("hide <feed> successful");
-}
-
-function hideSideBarRecommandations(){
-    document.getElementById('watch7-sidebar').remove();
-    console.log("hide <sidebar> successful");
-}
-
 function unhidePlayer(){
     document.getElementById('movie_player').setAttribute('style','');
     console.log('unhide <player> successful');
 }
 
 function hidePlayer(){
-    // TODO make the following two lines of code a function
-    // TODO make sure it does not play automatically
     // XXX occupying space is fine, if set to display:none
     // youtube will error out: base.js:2583 Uncaught TypeError: Cannot read property 'g' of null
     // maybe because of the rendering?
     var html5Video = document.getElementsByTagName('video')[0];
     html5Video.removeAttribute('src');
     html5Video.load();
-
-    // try{
-    //     // pause video
-    //     var html5Video = document.getElementsByTagName('video')[0]; // add a event listener
-    //     var config = {attributes:true}; // class = pause-mode, playing-mode, end-mode
-    //     var observer = new MutationObserver(function(array,instance){
-    //         logHelper(array,'html5 video element is modified');
-    //         console.log('try pause');
-    //         html5Video.pause();
-    //         var stringArray = moviePlayer.getAttribute('class').split(' ');
-    //         for(var i = 0; i < stringArray.length; i++){
-    //             if('paused-mode' == stringArray[i]){
-    //                 // TODO still hear 1 milisec of sound
-    //                 console.log('paused successful' + moviePlayer);
-    //                 instance.disconnect();
-    //             }
-    //         }
-    //     })
-    //     observer.observe(html5Video,config);
-    // }catch(e){
-    //     console.log('does not have html5Video');
-    //     console.log(e);
-    // }
 
     // hide video controls
     var moviePlayer = document.getElementById('movie_player');
@@ -96,28 +73,20 @@ function getPublisher(){
 }
 
 function hideEndScreen(){
-    try{
-        var videoPlayer = document.getElementById('movie_player');
-        var config = {attributes:true}; // class = pause-mode, playing-mode, end-mode
-        var observer = new MutationObserver(function(array,instance){
-            logHelper(array,'movie player is modified');
-            try{
-                // XXX endscreen is different for each video, need to run this code everytime
-                var target = videoPlayer.getElementsByClassName('html5-endscreen ytp-player-content videowall-endscreen')[0];
-                target.remove();
-                console.log('hide <endscreen> successful');
-                instance.disconnect(); // keep watching until we get a endscreen
-            }catch(e){
-                console.log('does not have endscreen')
-                console.log(e);
-            }
-        })
-        observer.observe(videoPlayer,config);
-    }catch(e){
-        console.log('does not have videoplayer');
-        // adblocker create too many load exceptions, so we use log to filter our exceptions
-        console.log(e);
-    }
+    var videoPlayer = document.getElementById('movie_player');
+    var config = {attributes:true}; // class = pause-mode, playing-mode, end-mode
+    var observer = new MutationObserver(function(array,instance){
+        logHelper(array,'movie player is modified');
+        try{
+            // XXX endscreen is different for each video, need to run this code everytime
+            videoPlayer.getElementsByClassName('html5-endscreen ytp-player-content videowall-endscreen')[0].remove();
+            instance.disconnect(); // keep watching until we get a endscreen
+        }catch(e){
+            console.log('does not have endscreen')
+            console.log(e);
+        }
+    });
+    observer.observe(videoPlayer,config);
 }
 
 function logHelper(mutationsRecords, logStr){
